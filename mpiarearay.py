ad71cc2a41493a9db09d0fe03eb65a33790bfdb0
raycu03 <44121622+raycu03@users.noreply.github.com>
2019-09-23 23:54:45 -0500

    Update mpiarearay.py

--- mpiarearay.py
+++ mpiarearay.py
@@ -1,61 +1,68 @@
+from mpi4py import MPI
 import numpy as np
 import matplotlib.pyplot as plt
-from mpi4py import MPI
-
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size() 
 
-
-def functionx(x):
-    
+def f(x):
     fxi = np.sqrt(x)*np.sin(x)
     return(fxi)
 
-def integral(a, b, tramos):
-    h = (b - a) / tramos
+
+def integral(a,b,n,h):
+    integral = (f(a) + f(b))/2.0
     x = a
-    suma = functionx(x)
-    for i in range(0, tramos - 1, 1):
+    for i in range(1,int(n)):
         x = x + h
-        suma = suma + 2 * functionx(x)
-    suma = suma + functionx(b)
-    area = h * (suma / 2)
-    return area    
-            
+        integral = integral + f(x)
+    return integral*h
+    
 
 a = 1
 b = 4
-tramos = 10
+n=10
+dest=0
+total=0
+integral_suma=0.0
 
+h = (b-a)/n 
+new_n = n/size 
 
+new_a = a + rank *new_n*h
+new_b = new_a + new_n*h
 
-print(integral(a, b, tramos))
+integ=integral(new_a,new_b,new_n,h)
 
- 
-for i in range(1, tramos):
+if rank == 0:
+    total = integ
+    for source in range(1,size):
+        integ = comm.recv(source=source)
+        print("PE ",rank,"<-",source,",",integ,"\n")
+        total = total + integ
+else :
+    print("maquina ",rank," area: ",integ,"\n")
+    comm.send(integ, dest=0)
+
+if (rank == 0):
+    print("tramos: ",n)
+    print("respuesta: ",total,"\n")
     
-    
-    print("tramos ", i, "area ", integral(a, b, i))
-muestras = tramos + 1
+
+muestras = n + 1
 xi = np.linspace(a, b, muestras)
-fi = functionx(xi)
-
-
-# Gráfica
-# Referencia función contínua
+fi = f(xi)
+    
 xig = np.linspace(a, b, muestras * 10)
-fig = functionx(xig)
+fig = f(xig)
 plt.plot(xig, fig)
 # Trapecios
 plt.fill_between(xi, 0, fi, color = 'y')
 plt.title('Integral: Regla de Trapecios')
 for i in range(0, muestras, 1):
     plt.axvline(xi[i], color = 'w')
-plt.plot(xi, fi, 'o') # puntos muestra
+plt.plot(xi, fi, 'o') 
 plt.xlabel('x')
 plt.ylabel('f(x)')
-plt.show()
-plt.savefig("i.jpg")
-
-print('tramos: ', tramos)
+plt.savefig("grafica.jpg")
